bf0e0819b220c6313795bff0fcf65666e4b94cef
Ana Gelez <ana@gelez.xyz>
2022-12-01 21:27:35 +0100

    Display an image in a Bevy window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(draw_board)
+        .run()
+}
+
+fn draw_board(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("background.png"),
+        ..Default::default()
+    });
 }
