5125277d31e86714eefa2f953d241c145d8a41c8
Edgar Onghena <dev@edgar.bzh>
2022-12-01 22:53:29 +0100

    Implement more card types

--- src/data/card_definition.rs
+++ src/data/card_definition.rs
@@ -3,8 +3,27 @@ use serde::Deserialize;
 #[derive(Debug, Deserialize)]
 pub struct CardDefinition {
     display_name: String,
+
+    #[serde(default)]
+    description: Option<String>,
+
+    /// Informations "IRL", pas utilisées dans le jeu
+    #[serde(default)]
+    info: Option<String>,
+
     icon: Option<String>,
 
     #[serde(default)]
-    color: [u8; 3],
+    category: CardCategory,
+}
+
+#[derive(Debug, Default, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum CardCategory {
+    #[default]
+    Uncategorized,
+    Building,
+    Prevention,
+    TreatmentRecipe,
+    Treatment,
 }
--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::path::Path;
+use walkdir::WalkDir;
 use crate::data::card_definition::CardDefinition;
 
 pub mod card_definition;
@@ -14,11 +15,13 @@ impl Data {
     pub fn load() -> Self {
         let mut cards = HashMap::new();
 
-        for file in std::fs::read_dir("data/cards").unwrap() {
+        for file in WalkDir::new("data/cards") {
             let file = file.unwrap();
-            let id = file.file_name().into_string().unwrap();
-            let file = std::fs::read(file.path()).unwrap();
-            cards.insert(id, toml::from_slice::<CardDefinition>(&file).unwrap());
+            if file.metadata().unwrap().is_file() {
+                let id = file.path().strip_prefix("data/cards/").unwrap().to_str().unwrap().strip_suffix(".toml").unwrap().to_string().replace("/", "_");
+                let file = std::fs::read(file.path()).unwrap();
+                cards.insert(id, toml::from_slice::<CardDefinition>(&file).unwrap());
+            }
         }
 
         Self {
