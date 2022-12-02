86b716b2d29d955ad69b85e069e8eeddc54ccb15
Ana Gelez <ana@gelez.xyz>
2022-12-02 01:54:11 +0100

    Load data from files

--- Carte.gd
+++ Carte.gd
@@ -1,18 +1,17 @@
 extends Node2D
 
+export var file : String
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var font
+var data
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	font = DynamicFont.new()
+	font.font_data = load("res://noto.ttf")
+	var f = File.new()
+	f.open("res://data/cards/" + file + ".json", File.READ)
+	data = parse_json(f.get_as_text())
 
 func _draw():
-	draw_rect(Rect2(0, 0, 100, 150), Color.azure)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	draw_rect(Rect2(0, 0, 100, 150), Color.blanchedalmond)
+	draw_string(font, Vector2(10, 20), data["display_name"], Color(0, 0, 0))
